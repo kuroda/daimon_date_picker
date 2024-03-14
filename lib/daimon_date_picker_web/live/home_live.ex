a6f815e31491bce2e5aa7fce5527889df609f4d9
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-03-15 08:40:06 +0900

    Impl. toggle_date_picker event handler

--- lib/daimon_date_picker_web/live/home_live.ex
+++ lib/daimon_date_picker_web/live/home_live.ex
@@ -5,7 +5,20 @@ defmodule DaimonDatePickerWeb.HomeLive do
     socket =
       socket
       |> assign(:selected_date, Date.utc_today())
+      |> assign(:activated, false)
 
     {:ok, socket, layout: {DaimonDatePickerWeb.Layouts, :app}}
+  end
+
+  def handle_event("toggle_date_picker", _params, socket) do
+    {:noreply, assign(socket, :activated, not socket.assigns.activated)}
+  end
+
+  defp date_picker(assigns) do
+    ~H"""
+    <div class="w-60 border mt-4 p-2 border-black">
+      Date Picker
+    </div>
+    """
   end
 end
