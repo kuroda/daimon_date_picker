159a7104adad8cfc0d99806994ec6ede7586b85a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-03-15 08:36:04 +0900

    Show text input with today's date as the initial value

--- lib/daimon_date_picker_web/live/home_live.ex
+++ lib/daimon_date_picker_web/live/home_live.ex
@@ -2,6 +2,10 @@ defmodule DaimonDatePickerWeb.HomeLive do
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:selected_date, Date.utc_today())
+
     {:ok, socket, layout: {DaimonDatePickerWeb.Layouts, :app}}
   end
 end
