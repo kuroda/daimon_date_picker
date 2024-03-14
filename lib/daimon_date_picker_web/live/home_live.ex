48e77a85d2b263a6510f38d529d8c3c1d9fdee79
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-03-15 08:31:56 +0900

    Apply :app layout to the home page

--- lib/daimon_date_picker_web/live/home_live.ex
+++ lib/daimon_date_picker_web/live/home_live.ex
@@ -1,3 +1,7 @@
 defmodule DaimonDatePickerWeb.HomeLive do
   use Phoenix.LiveView
+
+  def mount(_params, _session, socket) do
+    {:ok, socket, layout: {DaimonDatePickerWeb.Layouts, :app}}
+  end
 end
