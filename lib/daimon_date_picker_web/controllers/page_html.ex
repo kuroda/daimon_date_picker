5c2d785364c46b5120a636b3eae0b487ada4b3a7
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-03-15 08:18:39 +0900

    Remove controllers; Clean up router.ex

--- lib/daimon_date_picker_web/controllers/page_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule DaimonDatePickerWeb.PageHTML do
-  use DaimonDatePickerWeb, :html
-
-  embed_templates "page_html/*"
-end
--- test/daimon_date_picker_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule DaimonDatePickerWeb.PageControllerTest do
-  use DaimonDatePickerWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
