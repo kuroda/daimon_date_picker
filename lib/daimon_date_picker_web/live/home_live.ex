c85e1001a7283299c982b68cd403632ee3d2b561
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-03-15 08:54:49 +0900

    Show current month and prev/next buttons

--- lib/daimon_date_picker_web/live/home_live.ex
+++ lib/daimon_date_picker_web/live/home_live.ex
@@ -5,6 +5,7 @@ defmodule DaimonDatePickerWeb.HomeLive do
     socket =
       socket
       |> assign(:selected_date, Date.utc_today())
+      |> assign(:current_month, Date.beginning_of_month(Date.utc_today()))
       |> assign(:activated, false)
 
     {:ok, socket, layout: {DaimonDatePickerWeb.Layouts, :app}}
@@ -16,8 +17,12 @@ defmodule DaimonDatePickerWeb.HomeLive do
 
   defp date_picker(assigns) do
     ~H"""
-    <div class="w-60 border mt-4 p-2 border-black">
-      Date Picker
+    <div class="w-60 border mt-4 p-2 border-black grid grid-cols-7 gap-1">
+      <div class="col-span-7 p-1 flex justify-between">
+        <button type="button" class="underline">Prev</button>
+        <span><%= Calendar.strftime(@current_month, "%B %Y") %></span>
+        <button type="button" class="underline">Next</button>
+      </div>
     </div>
     """
   end
